bac49f46638c2b0fad27a5f386a1f32ce64ed305
Musah <kamaramusa56@gmail.com>
2024-10-04 01:23:22 +0300

    made some changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom/dist";
-
+import { BrowserRouter } from "react-router-dom"; 
 const client = new ApolloClient({
   uri: "https://countries.trevorblades.com/graphql",
   cache: new InMemoryCache(),
